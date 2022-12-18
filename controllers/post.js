15e5f4de0b837760d12c0024325de3760c9175f0
Ben Jeckson <thanhtikexaw@gmail.com>
2022-12-18 16:58:38 +0630

    Updated Posts Function

--- controllers/post.js
+++ controllers/post.js
@@ -1,12 +1,22 @@
+const DB = require('../dbs/posts');
+const Helper = require('../utils/helper');
+
 const all = async (req, res, next) => {
-    res.json({msg:"All Posts"});
+    let posts = await DB.find();
+    Helper.Fmsg(res, "All Posts", posts);
 }
 const get = async (req, res, next) => {
-    res.json({msg:"Single Post"});
+    let post = await DB.findById(req.params.id);
+    if(post) {
+        Helper.Fmsg(res, "Single Post", post);
+    } else {
+        next(new Error("No post with that ID")); 
+    }
 }
 
 const post = async (req, res, next) => {
-    res.json({msg:"Create Post"});
+    let result = await new DB(req.body).save();
+    Helper.Fmsg(res, "Post Created", result);
 }
 
 const patch = async (req, res, next) => {
